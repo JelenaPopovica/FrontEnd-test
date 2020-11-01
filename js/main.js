4db084120554ab371b061d0870522ad67fab89b9
JelenaPopovica <popovica.jelena@gmail.com>
2020-11-01 17:41:36 +0200

    submit-btn
    alingment update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function(){
 
+  
   document.getElementById('checkbox').
   addEventListener('click', function(event){
     const isChecked = document.getElementsByName('checkbox')[0].checked;
